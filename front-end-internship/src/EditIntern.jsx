957eaaa6f201c5bb4ea62d3e0e6ab9dc2c994774
dmalenik <dmalenik@gmail.com>
2022-05-01 15:15:48 +0200

    22_05_01_internData was taken and displayed

--- front-end-internship/src/EditIntern.jsx
+++ front-end-internship/src/EditIntern.jsx
@@ -1,28 +1,49 @@
-import React, { useEffect } from 'react';
-import { useParams } from 'react-router';
-import { NavLink } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import { NavLink } from "react-router-dom";
 
 const EditIntern = () => {
-    const { id } = useParams();
+  // added
+  const [internData, setInternData] = useState();
+  console.log(internData);
+  const { id } = useParams();
 
-    useEffect(() => {
-        //TODO: get intern from REST api http://localhost:3001/interns/:id
-        console.log(`I want to get intern with id: ${id}!`)
-    }, [id]);
+  // added
+  const getIntern = async () => {
+    let url = `http://localhost:3001/interns/${id}`;
+    let response = await fetch(url);
+    let internData = await response.json();
+    setInternData(internData);
+  };
 
-    return (
-        <div>
-            <NavLink to="/">Back to list </NavLink>
-            <form>
-                <label>Name</label>
-                <input type="text" name="name" />              
-                <label>Email</label>
-                <input type="text" name="email" />
-                
-                <input type="submit" value="Submit" />
-            </form>
-        </div>
-    );
+  useEffect(() => {
+    //TODO: get intern from REST api http://localhost:3001/interns/:id
+    console.log(`I want to get intern with id: ${id}!`);
+
+    getIntern();
+  }, [id]);
+
+  return (
+    <div>
+      <NavLink to="/">Back to list </NavLink>
+      <form>
+        <label>Name</label>
+        <input
+          type="text"
+          name="name"
+          value={internData ? internData.name : ""}
+        />
+        <label>Email</label>
+        <input
+          type="text"
+          name="email"
+          value={internData ? internData.email : ""}
+        />
+
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
 };
 
 export default EditIntern;
