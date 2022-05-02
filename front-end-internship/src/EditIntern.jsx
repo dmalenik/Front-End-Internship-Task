0753199b32457884ab2681d7e83aa49395f596f6
dmalenik <dmalenik@gmail.com>
2022-05-02 23:09:14 +0200

    22_05_02_email validation added

--- front-end-internship/src/EditIntern.jsx
+++ front-end-internship/src/EditIntern.jsx
@@ -1,19 +1,18 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import { NavLink } from "react-router-dom";
+import isEmail from "validator/lib/isEmail";
 
 const EditIntern = () => {
-  // added
-  const [internData, setInternData] = useState();
-  console.log(internData);
+  const [intern, setIntern] = useState();
   const { id } = useParams();
 
-  // added
   const getIntern = async () => {
     let url = `http://localhost:3001/interns/${id}`;
     let response = await fetch(url);
-    let internData = await response.json();
-    setInternData(internData);
+    let intern = await response.json();
+
+    setIntern(intern);
   };
 
   useEffect(() => {
@@ -28,18 +27,28 @@ const EditIntern = () => {
       <NavLink to="/">Back to list </NavLink>
       <form>
         <label>Name</label>
-        <input
-          type="text"
-          name="name"
-          value={internData ? internData.name : ""}
-        />
+        <input type="text" name="name" value={intern?.name || ""} required />
         <label>Email</label>
         <input
           type="text"
           name="email"
-          value={internData ? internData.email : ""}
+          value={intern ? (isEmail(intern.email) ? intern.email : "") : ""}
+          required
         />
-
+        <label>Internship Start</label>
+        <input
+          type="date"
+          name="internshipStart"
+          value={intern?.internshipStart.slice(0, 10) || ""}
+          required
+        />
+        <label>Internship End</label>
+        <input
+          type="date"
+          name="internshipEnd"
+          value={intern?.internshipEnd.slice(0, 10) || ""}
+          required
+        />
         <input type="submit" value="Submit" />
       </form>
     </div>
